0cb581cc855093ec37b1dde53a47570238340b79
Ankur Mahajan <mahajanankur.nith@gmail.com>
2023-03-09 23:18:31 +0530

    updated config profiling with default.json as default file and env variable prefrence.

--- src/configs/config_loader.py
+++ src/configs/config_loader.py
@@ -1,9 +1,12 @@
 import os, configparser, json
 import logging
 logger = logging.getLogger("flask-app")
+from dotenv import load_dotenv
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 from utils.json_util import JsonUtils
+
+load_dotenv()  # load environment variables from .env file
 
 class ConfigLoader:
     _instance = None
@@ -16,32 +19,31 @@ class ConfigLoader:
         return cls._instance
 
     @staticmethod
-    def load_config():
+    def load_config(env = "local"):
         try:
             # Determine the environment
-            if 'ENV' in os.environ:
-                env = os.environ['ENV']
-            else:
-                env = 'dev'
-
+            environment = os.environ.get('ENV', env)
+            logger.error(("Env = {en} and environment = {envv}").format(en=env, envv = environment))
             # Load the default configuration file
             with open(f'{dir_path}/default.json', 'r') as f:
                 default_configs = json.load(f)
-
-            # Load the environment-specific configuration file
-            with open(f'{dir_path}/{env}.json', 'r') as f:
-                env_config = json.load(f)
-
-            # Merge the default and dynamic properties.
-            merged_configs = JsonUtils.merge_json(default_configs, env_config)
+            
+            if environment != "local":
+                # Load the environment-specific configuration file
+                with open(f'{dir_path}/{environment}.json', 'r') as f:
+                    env_config = json.load(f)
+                # Merge the default and dynamic properties.
+                merged_configs = JsonUtils.merge_json(default_configs, env_config)
+            else:
+                merged_configs = default_configs
             
             return merged_configs
 
         except (FileNotFoundError, json.JSONDecodeError, configparser.Error) as e:
-            raise Exception(f"Error loading configuration: {str(e)}")
+            raise Exception(f"Error loading configurations: {str(e)}")
 
     @staticmethod
-    def get_config():
+    def get_config(env = "local"):
         if ConfigLoader._config is None:
-            ConfigLoader._config = ConfigLoader.load_config()
+            ConfigLoader._config = ConfigLoader.load_config(env=env)
         return ConfigLoader._config
